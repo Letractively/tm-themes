82c708603336927e6bcd35f394796da7e5c734b2
phi1618.jp <phi1618.jp@2e0528b9-b971-f9b1-467c-821e39252229>
2011-09-20 16:45:50 +0000

    tm-style 追加

---
+++ trunk/tm-style/footer.php
@@ -0,0 +1,8 @@
+
+            <footer id="footer">
+                <p>
+                    Copyright &copy; <?php bloginfo('name'); ?>, All Rights Reserved.
+                </p>
+            </footer>
+            <?php wp_footer(); ?>
+            
