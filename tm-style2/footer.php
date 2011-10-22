a654b272b1bec0da420c825c68ec13639f250f54
phi1618.jp <phi1618.jp@2e0528b9-b971-f9b1-467c-821e39252229>
2011-10-22 05:50:27 +0000

    サイドボックス上の演出を追加 コメント系のタイトル色を変更

--- tm-style2/footer.php
+++ tm-style2/footer.php
@@ -1,8 +1,8 @@
 <footer id="footer">
     <aside id="footer-aside" class="clearfix">
-        <?php dynamic_sidebar('footer-widget00'); ?>
-        <?php dynamic_sidebar('footer-widget01'); ?>
-        <?php dynamic_sidebar('footer-widget02'); ?>
+        <div class="footer-widget"><?php dynamic_sidebar('footer-widget00'); ?></div>
+        <div class="footer-widget"><?php dynamic_sidebar('footer-widget01'); ?></div>
+        <div class="footer-widget"><?php dynamic_sidebar('footer-widget02'); ?></div>
     </aside>
     <address>
         Copyright &copy; <?php bloginfo('name'); ?>. All Rights Reserved.
